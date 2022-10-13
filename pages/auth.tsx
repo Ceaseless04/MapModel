fa1b7efab42a7f3bcd823ea66751f86bde32002b
Coral <coral@Coral-computar.attlocal.net>
2022-10-13 18:53:21 -0400

    fonts changes

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,38 +1,52 @@
 // this is the authentication page
 import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
-const Authentication=()=>{
-    return(
-        <div>
-            <button onClick={submitEmail}>Submit</button>
-            {/* step one: email input */}
-            <div>
-                <form>
-                    <input type="text"></input>
-                    <button type="submit"></button>
-                </form>
-            </div>
-           {/* step two: image input */}
-           <div>
-                <form>
-                    <input type="image"></input>
-                    {/* submit everything to firebase on this step */}
-                    <button type="submit"></button>
-                </form>
-            </div>
-        </div>
-    )
+import global from "../styles/Global.module.scss";
+const Authentication = () => {
+  return (
+    <div className={global.font}>
+      <button onClick={submitEmail}>Submit</button>
+      {/* step one: email input */}
+      <div>
+        <h1>add email</h1>
+        <form>
+          <input type="text"></input>
+          <button type="submit"></button>
+        </form>
+      </div>
+      {/* step two: image input */}
+      <div>
+        <h1>add information</h1>
+        <form>
+        <input type="text" placeholder="name"></input> 
+        <input type="text" placeholder="Major"></input>
+        <input type="text" placeholder="country"></input>
+         
 
-}
+          {/* submit everything to firebase on this step */}
+          <button type="submit"></button>
+        </form>
+      </div>
+       {/* step two: image input */}
+       <div>
+        <h1>add image</h1>
+        <form>
+          <input type="image"></input>
+          {/* submit everything to firebase on this step */}
+          <button type="submit"></button>
+        </form>
+      </div>
+    </div>
+  );
+};
 const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:3000/',
-    // This must be true.
-    handleCodeInApp: true,
-    
-  };
-  function submitEmail() {
-    let email = "kristian120304@gmail.com"
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be in the authorized domains list in the Firebase Console.
+  url: "http://localhost:3000/",
+  // This must be true.
+  handleCodeInApp: true,
+};
+function submitEmail() {
+  let email = "kristian120304@gmail.com";
 
   const auth = getAuth();
   sendSignInLinkToEmail(auth, email, actionCodeSettings)
@@ -40,8 +54,8 @@ const actionCodeSettings = {
       // The link was successfully sent. Inform the user.
       // Save the email locally so you don't need to ask the user for it again
       // if they open the link on the same device.
-      console.log("hello", email)
-      window.localStorage.setItem('emailForSignIn', email);
+      console.log("hello", email);
+      window.localStorage.setItem("emailForSignIn", email);
       // ...
     })
     .catch((error) => {
@@ -49,7 +63,6 @@ const actionCodeSettings = {
       const errorMessage = error.message;
       // ...
     });
-  }
+}
 
-  
-export default Authentication
+export default Authentication;
