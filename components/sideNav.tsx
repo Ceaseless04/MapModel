b2c86677a7f701c693d334104788cdcfb7f7defa
Leon Kipkoech <leonkipkip@gmail.com>
2022-10-14 13:29:35 -0400

    side nav added

--- components/sideNav.tsx
+++ components/sideNav.tsx
@@ -1,9 +1,35 @@
+import React from 'react';
 import  styles  from '../styles/Side.module.scss'
+import globals from '../styles/Global.module.scss'
 
-const SideNav=()=>{
+type props = {
+    imageUrl: string;
+    name: string;
+    city: string;
+    country: string;
+    course: string;
+}
+
+const SideNav=({imageUrl,name,city,country,course}:props)=>{
+    let [state,open]=React.useState(true)
+    function menu(){
+        open(state=!state)
+    }
     return(
-        <div className={styles.container}>
-
+        <div className={`${styles.container} ${state?styles.open:styles.close}` }>
+            {/* <image></image> */}
+            <div className={styles.closeOpenBtn}>
+                <button onClick={()=>{menu()}}>{!state?"Open":"Close"}</button>
+            </div>
+            <div className={`${state?styles.image_container:globals.hidden}`  }>
+                 {imageUrl}
+            </div>
+            <div className={`${state?styles.container_bottom:globals.hidden}`}>
+            <h1> {name} </h1>
+            <h2>{city}, {country}</h2>
+            <h2>{course}</h2>
+            </div>
+            
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,43 @@
-import { setDefaultResultOrder } from 'dns';
-import type { NextPage } from 'next'
-import React from 'react';
-import { useEffect } from 'react';
-import { urlToHttpOptions } from 'url';
-import Error from '../components/error'
-import data from '../public/Major_Names.json'
+import type { NextPage } from "next";
+import React from "react";
+import Error from "../components/error";
+import SideNav from "../components/sideNav";
+import style from "../styles/Home.module.scss"
+
 
 const Home: NextPage = () => {
-  let [err, setError] = React.useState({active: false,code: 0})
- 
- 
-  async function error(code: number){
-    setError({active: !err.active,code: code})
-    if(!err.active){
-      await new Promise(f => setTimeout(f, 3000));
+  let [err, setError] = React.useState({ active: false, code: 0 });
+
+  async function error(code: number) {
+    setError({ active: !err.active, code: code });
+    if (!err.active) {
+      await new Promise((f) => setTimeout(f, 3000));
       // restore defaults
-      setError({active: err.active,code: code})
+      setError({ active: err.active, code: code });
     }
   }
 
-  function fetchMajorLit(element: any){
-
-    for(let major of data["MAIN"]){
-      // let ele = <option></option> as unknown as HTMLOptionElement;
-      // ele.value = major['Major Name']
-      element.appendChild(<option>{major['Major Name']}</option> )
-      
-    }
-  }
+  
 
   // to call error call error(code)
   // the code is in errorCodes.tsx
 
   return (
     <div>
-      <div>
-       
+      <div className={style.parent}>
+        <SideNav imageUrl="imageUrl" name="Harry Potter" course="magic" city="hogwarts" country="United Kingdom"></SideNav>
+        <div>
+          <div></div>
+          <button
+            onClick={() =>  error(1)} >
+            activate error
+          </button>
+        </div>
       </div>
-      <button onClick={()=>{fetchMajorLit(document.getElementById('test'))}}>
-        activate error
-      </button>
-     
-      <Error code={err.code} boolean={err.active}></Error>   
-    </div>
-  )
-}
 
-export default Home
+      <Error code={err.code} boolean={err.active}></Error>
+    </div>
+  );
+};
+
+export default Home;
