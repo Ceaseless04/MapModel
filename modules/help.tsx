2efb0ee77ae61e2d9fa52b0f68b152cfabc8cc7a
Ceaseless04 <kristian120304@gmail.com>
2022-11-12 13:11:45 -0500

    styling lol

--- modules/help.tsx
+++ modules/help.tsx
@@ -5,18 +5,19 @@ export const help:step[] =[
         title: "step1",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/storage%2F915c859e580ed78052cc9615fe08a36d.jpg?alt=media&token=0fd064b9-2c2c-4b29-8b5a-8dbbcb522d85",
         imageAria: "step 1 for mapme",
-        details:" Start by creating an account with us and verify it"
+        details:"Create your account and verify it with your email"
     },
     {
         title: "step2",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/storage%2F915c859e580ed78052cc9615fe08a36d.jpg?alt=media&token=0fd064b9-2c2c-4b29-8b5a-8dbbcb522d85",
         imageAria: "step 1 for mapme",
-        details:" Start by creating an account with us and verify it"
+        details:"Complete form and upload your profile picture"
     },
     {
         title: "step3",
         imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/storage%2F915c859e580ed78052cc9615fe08a36d.jpg?alt=media&token=0fd064b9-2c2c-4b29-8b5a-8dbbcb522d85",
         imageAria: "step 1 for mapme",
-        details:" Start by creating an account with us and verify it"
+        details:"Feel free to look around the map and learn about different cultures"
     }
+
 ]
