cd6912f8dd761015639c3d9ab89c712c350eb3fc
Ceaseless04 <kristian120304@gmail.com>
2022-11-13 00:22:09 -0500

    finished error codes

--- modules/errorCodes.tsx
+++ modules/errorCodes.tsx
@@ -2,9 +2,13 @@ export const codes={
     1: "We were unable to verify that email. (Pst: Try checking your inbox and spam folders)",
     2: "Image has not been uploaded. Please try again",
     3: "Successfully added to the map",
-    4: "An Input is empty, please check for it",
+    4: "An Input(s) is empty, please check for it",
     5: "Please enter a valid email",
     6: "Email verification link has been sent",
     7: "City is Empty, Make  sure to search for it",
-    8: "please select an image"
+    8: "please select an image",
+    9: "We do not seem to recognize that name. What is your name?",
+    10: "Red flag, do not forget to select your major!",
+    11: "We are missing some information. What is your country again?",
+    12: "OOPS. Can you select the city again?"
 }
