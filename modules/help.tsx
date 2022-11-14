3c2ef3d95944c1b296c15cb43327d1a223acd865
Ceaseless04 <kristian120304@gmail.com>
2022-11-14 14:51:39 -0500

    final touches to how to images

--- modules/help.tsx
+++ modules/help.tsx
@@ -3,19 +3,19 @@ import {step} from '../models/step'
 export const help:step[] =[
     {
         title: "step1",
-        imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/storage%2F915c859e580ed78052cc9615fe08a36d.jpg?alt=media&token=0fd064b9-2c2c-4b29-8b5a-8dbbcb522d85",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/unnamed.png?alt=media&token=32a74304-6b19-4983-b152-db2a3045495a",
         imageAria: "step 1 for mapme",
         details:"Create your account and verify it with your email"
     },
     {
         title: "step2",
-        imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/storage%2F915c859e580ed78052cc9615fe08a36d.jpg?alt=media&token=0fd064b9-2c2c-4b29-8b5a-8dbbcb522d85",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/unnamed%20(1).png?alt=media&token=42fe2b6f-6dac-49c0-b6e5-b2da9c8ddfe7",
         imageAria: "step 1 for mapme",
         details:"Complete form and upload your profile picture"
     },
     {
         title: "step3",
-        imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/storage%2F915c859e580ed78052cc9615fe08a36d.jpg?alt=media&token=0fd064b9-2c2c-4b29-8b5a-8dbbcb522d85",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/mapme-66db2.appspot.com/o/unnamed%20(2).png?alt=media&token=89a21b6f-e985-4660-ad10-effb812c6c0c",
         imageAria: "step 1 for mapme",
         details:"Feel free to look around the map and learn about different cultures"
     }
